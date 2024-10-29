37b0961be62d88a08905f7c5d39ae2d5a482e94c
JeanPaulSidi <paulsidi@outlook.com>
2024-10-28 22:04:57 -0700

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANK_TEST
+{
+    public class BankAccount
+    {
+        string _name;
+        string _password;
+        decimal _balance;
+
+
+        private BankAccount(string Name, string Password, decimal Balance) 
+        {
+            _name = Name;
+            _password = Password;
+            _balance = Balance;
+        }
+
+        public BankAccount() { }
+
+        public decimal Balance 
+        {
+            get { return _balance; }
+        }
+
+        public string Name 
+        {
+            get { return _name; }
+        }
+
+        public BankAccount Login(string Name, string Password) 
+        {
+            List<BankAccount> accounts = new List<BankAccount>();
+
+            accounts.Add(new BankAccount("bert", "sesame1", 200m));
+            accounts.Add(new BankAccount("ernie", "Sesame2", 1000m));
+        
+            bool CredentialsMatch;
+            int i = 0;
+            CredentialsMatch = false;
+
+            do
+            {
+                if (Name == accounts[i]._name && Password == accounts[i]._password)
+                {
+                    CredentialsMatch = true;
+                }
+                else 
+                {
+                    i++;
+                }
+
+            } while( (!CredentialsMatch) && (i != accounts.Count) );
+            
+            if (CredentialsMatch) 
+            {
+                return accounts[i];
+            }
+            else 
+            {
+                return null;
+            }
+        }
+
+        public string Deposit(decimal Amount) 
+        {
+            string Message;
+
+            _balance += Amount;
+            Message = ("succes. new balance:");
+
+            return Message + _balance.ToString("C");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BANK_TEST
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount UserAccount = new BankAccount();
+            string UserName;
+            string UserPassword;
+            decimal amount;
+            string Message;
+
+            Console.Write("Name: ");
+            UserName = Console.ReadLine();
+            Console.Write("password: ");
+            UserPassword = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.Write("login in progress.......");
+
+            UserAccount = UserAccount.Login(UserName, UserPassword);
+            Console.WriteLine();
+
+            if (UserAccount != null)
+            {
+                Console.WriteLine("welcome back " + UserAccount.Name);
+                Console.WriteLine();
+
+                Console.Write("Balance: " + UserAccount.Balance.ToString("C"));
+            }
+            else 
+            {
+                Console.WriteLine("error");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("amount:");
+            amount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Message = UserAccount.Deposit(amount);
+            Console.WriteLine(Message);
+
+            
+        }
+    }
+}
